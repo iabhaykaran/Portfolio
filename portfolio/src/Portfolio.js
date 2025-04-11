16571dc9b8888104228cd4010afb8a9954cab4c0
iabhaykaran <iamabhaykaran@gmail.com>
2025-04-11 14:39:12 +0530

    msg

---
+++ portfolio/src/Portfolio.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Portfolio.css";
+
+const projects = [
+  {
+    name: "Youtube Clone",
+    link: "https://youtube-clone-eight-cyan.vercel.app/",
+    thumbnail: "yt.avif",
+  },
+  {
+    name: "Project Two",
+    link: "https://project-two.vercel.app",
+    thumbnail:
+      "https://image.thum.io/get/width/600/https://project-two.vercel.app",
+  },
+  {
+    name: "Project Three",
+    link: "https://project-three.vercel.app",
+    thumbnail:
+      "https://image.thum.io/get/width/600/https://project-three.vercel.app",
+  },
+];
+
+function Portfolio() {
+  return (
+    <div className="portfolio-container">
+      {projects.map((project, index) => (
+        <div
+          key={index}
+          className="project-card"
+          onClick={() => window.open(project.link, "_blank")}
+        >
+          <img
+            src={project.thumbnail}
+            alt={project.name}
+            className="project-image"
+          />
+          <div className="project-details">
+            <h3>{project.name}</h3>
+            <button className="open-button">Open Project 🚀</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Portfolio;
